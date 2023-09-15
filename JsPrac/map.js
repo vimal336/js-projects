672cfc88042630451f58a1053f11cf517d0e51cc
vimal336 <asvimal336@gmail.com>
2023-09-15 22:29:53 +0530

    map method

---
+++ JsPrac/map.js
@@ -0,0 +1,8 @@
+const numbers = [1, 4, 9];
+const roots = numbers.map((num) => Math.sqrt(num));
+
+console.log(roots);
+console.log(numbers);
+
+// roots is now     [1, 2, 3]
+// numbers is still [1, 4, 9]
