7fdcd44b6051598a52f81f3b6bab00fe97ab2674
vimal336 <asvimal336@gmail.com>
2023-09-16 20:52:10 +0530

    js async method

--- JsPrac/Async.js
+++ JsPrac/Async.js
@@ -1,29 +1,22 @@
-// const fet = fetch("https://jsonplaceholder.typicode.com/todos")
-//   .then((res) => res.json())
-//   .then((data) => console.log(data));
- 
-//   console.log(fet);
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
 
-async function logMovies() {
-    const response = await fetch("http://example.com/movies.json");
-    const movies = await response.json();
-    console.log(movies);
-  }
 
-  console.log('hello')
 
-  function resolveAfter2Seconds(x) {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(x);
-      }, 2000);
-    });
-  }
+
+  // function resolveAfter2Seconds(x) {
+  //   return new Promise((resolve) => {
+  //     setTimeout(() => {
+  //       resolve(x);
+  //     }, 2000);
+  //   });
+  // }
   
-  async function f1() {
-    const x = await resolveAfter2Seconds(10);
-    console.log(x); // 10
-  }
+  // async function f1() {
+  //   const x = await resolveAfter2Seconds(10);
+  //   console.log(x); // 10
+  // }
   
-  f1();
+  // f1();
   
