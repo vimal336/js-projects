37cc45580c528a7caade023e1e55c7999cb4a5d1
vimal336 <asvimal336@gmail.com>
2023-09-16 21:15:00 +0530

    js async await

--- JsPrac/Async.js
+++ JsPrac/Async.js
@@ -6,8 +6,8 @@
 
   async function getTodos() {
     const res = await fetch("https://jsonplaceholder.typicode.com/todos")
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+    const data = await res.json();
+    console.log(data);
   }
 
   getTodos();
