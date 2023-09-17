d166f6e70545916343e60ca47ebb3e9c458717df
vimal336 <asvimal336@gmail.com>
2023-09-17 22:49:13 +0530

    js filter method

--- JsPrac/filter.js
+++ JsPrac/filter.js
@@ -4,4 +4,17 @@ function isBigEnough(value) {
   
   const filtered = [12, 5, 8, 130, 44].filter(isBigEnough);
   // filtered is [12, 130, 44]
+
   
+  const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+function isPrime(num) {
+  for (let i = 2; num > i; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
+}
+
+
