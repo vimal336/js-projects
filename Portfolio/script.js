1da93baad5e8c449a8cd004e05bd0f3ecc512ac2
vimal336 <asvimal336@gmail.com>
2023-10-21 22:48:46 +0530

    my new portfolio project

---
+++ Portfolio/script.js
@@ -0,0 +1,55 @@
+// Script File
+
+// Home Section Script Starts
+var typingText = document.querySelector('.text2');
+var myArray = 
+["Web Developer", "Programmer"];
+var arrayIndex = 1;
+
+function textReplace(){
+	setInterval(timer, 5000);
+	function timer(){
+		if(arrayIndex < myArray.length){
+			typingText.innerHTML = myArray[arrayIndex];
+			arrayIndex = arrayIndex + 1;
+		}
+		else{
+			arrayIndex = 0;
+			typingText.innerHTML = myArray[arrayIndex];
+			arrayIndex = arrayIndex + 1;
+		}
+	}
+}
+textReplace();
+
+var menuBtn = document.querySelector('.navbar .menu-btn');
+var menuList = document.querySelector('.navbar .nav-list');
+var menuListItems = document.querySelectorAll('.nav-list li a');
+
+menuBtn.addEventListener('click', addActiveClass);
+
+function addActiveClass(){
+	menuList.classList.toggle('active');
+}
+
+for(var i = 0; i < menuListItems.length; i++){
+	menuListItems[i].addEventListener('click', menuItemClicked);
+}
+
+function menuItemClicked(){
+	menuList.classList.remove('active');
+}
+
+
+var homeSection = document.querySelector('.home');
+window.addEventListener('scroll', pageScrollFunction);
+window.addEventListener('load', pageScrollFunction);
+
+function pageScrollFunction(){
+	if(window.scrollY > 80){
+		homeSection.classList.add('active');
+	}
+	else{
+		homeSection.classList.remove('active');
+	}
+}
