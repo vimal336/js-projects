00af43382f063e4e12c3a38c2b2ba3436488ee07
vimal336 <asvimal336@gmail.com>
2023-10-25 14:06:13 +0530

    my new portfolio project

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -55,20 +55,7 @@ function pageScrollFunction(){
 }
 
 
-var $galleryContainer = $('.gallery').isotope({
-	itemSelector: '.item',
-	  layoutMode: 'fitRows'
-});
 
-$('.button-group .button').on('click', function(){
-	$('.button-group .button').removeClass('active');
-	$(this).addClass('active');
-
-	var value = $(this).attr('data-filter');
-	$galleryContainer.isotope({
-		filter: value 
-	})
-})
 
 // scroll to top code
 
