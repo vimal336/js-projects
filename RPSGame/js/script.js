8157e313b49ef6609b53981e4b3bebd1ae41d663
vimal336 <asvimal336@gmail.com>
2023-12-26 20:19:04 +0530

    rock paper scisssor game

--- RPSGame/js/script.js
+++ RPSGame/js/script.js
@@ -9,13 +9,6 @@ function reload() {
   const currentDate = new Date();
   const diff = newYear - currentDate;
 
-  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
-  const h = Math.floor((diff / 1000 / 60 / 60) % 24);
-  const m = Math.floor((diff / 1000 / 60) % 60);
-  const s = Math.floor((diff / 1000) % 60);
-
-  days.innerHTML=d<10?"0"+d:d;
-  hours.innerHTML=h<10?"0"+h:h;
 
 
 
