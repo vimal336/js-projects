ac9efbf1aed9d49d324c368a81e9c4973d0e5096
vimal336 <asvimal336@gmail.com>
2024-01-15 22:22:17 +0530

    rock paper scisssor game

--- RPSGame/js/script.js
+++ RPSGame/js/script.js
@@ -14,5 +14,5 @@ nextRoundButton = document.querySelector('#nextRoundButton');
 initButton = document.querySelector('#initButton');
 
 gameControls = document.querySelectorAll('.gameControls')
-gameControls = document.querySelectorAll('.gameControls')
+userControls = document.querySelectorAll('.gameControls')
 
