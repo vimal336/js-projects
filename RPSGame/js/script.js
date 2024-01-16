f518fb07e40134bd2fe240d33c095592bc67bfac
vimal336 <asvimal336@gmail.com>
2024-01-16 22:12:06 +0530

    rock paper scisssor game

--- RPSGame/js/script.js
+++ RPSGame/js/script.js
@@ -18,4 +18,4 @@ userControls = document.querySelectorAll('.userControls');
 computerControls = document.querySelectorAll('.computerControls');
 
 roundID = document.querySelector('#roundID');
-roundsSpan = roundID.querySelector('span')
+roundsSpan = roundID.querySelector('span');
