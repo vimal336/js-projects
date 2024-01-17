92b8a9c975705d7c376a701a17981126246dc989
vimal336 <asvimal336@gmail.com>
2024-01-17 22:33:39 +0530

    rock paper scisssor game

--- RPSGame/js/script.js
+++ RPSGame/js/script.js
@@ -22,4 +22,4 @@ roundsSpan = roundID.querySelector('span');
 
 countDown = roundID.querySelector('#countDown');
 userFireWork = document.querySelector('#userFireWork');
-userFireWork = document.querySelector('#userFireWork')
+userFireWork = document.querySelector('#userFireWork');
