8e3a614ba56751cd709f2b57b2ea2fa1e3c23388
vimal336 <asvimal336@gmail.com>
2024-01-29 13:22:48 +0530

    rock paper scisssor game

--- RPSGame/js/script.js
+++ RPSGame/js/script.js
@@ -104,7 +104,7 @@ function select(userInput){
   isGameControlKeysEnabled = false;
 
   if(userInput === computerInput ){
-     countDown.innerText = "Draw";
+     countDown.innerText = "Draw!";
   }else if{
     (userInput === 1 & computerInput === 2)
     (userInput === 2 & computerInput === 1)
