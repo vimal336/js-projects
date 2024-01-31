d900313f1d8b4e8163b31d7a7f52b98c3f9a6941
vimal336 <asvimal336@gmail.com>
2024-01-31 20:36:29 +0530

    rock paper scisssor game

--- RPSGame/js/script.js
+++ RPSGame/js/script.js
@@ -133,6 +133,6 @@ function gameOver(){
   const computerScoreValue = +this.computerScore.innerText;
 
   if (userScoreValue == computerScoreValue){
-    countDown.init
+    countDown.innerText
   }
 }
