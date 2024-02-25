15e96a5dc5f4805422bf3c3ebf5fa9bde9741bce
vimal336 <asvimal336@gmail.com>
2024-02-25 20:34:51 +0530

    react hooks

--- navbar/js/script.js
+++ navbar/js/script.js
@@ -1,6 +1,4 @@
 const days=document.querySelector('#days');
-const hours=document.querySelector('#hours');
-const minutes=document.querySelector('#minutes');
-const seconds=document.querySelector('#seconds');
+
 
 
