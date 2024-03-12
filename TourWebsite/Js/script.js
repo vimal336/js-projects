4c24a27aee95331abbccdbabd765aec5de5b58df
vimal336 <asvimal336@gmail.com>
2024-03-12 10:46:21 +0530

    tour website using html and css

--- TourWebsite/Js/script.js
+++ TourWebsite/Js/script.js
@@ -3,5 +3,5 @@ const nav = document.querySelector('.nav');
 const navList = document.querySelector('nav ul');
 
 menu.addEventListener('click', ()=>{
-  menuList.classList.toggle('active')
+  navList.classList.toggle('active')
 })
