d3149ee9422215a70b6a83f7af8674d449a2fb04
vimal336 <asvimal336@gmail.com>
2024-03-18 16:51:11 +0530

    tour website using html and css

--- TourWebsite/Js/script.js
+++ TourWebsite/Js/script.js
@@ -6,3 +6,38 @@ closeBtn.addEventListener('click', ()=>{
   nav.classList.toggle('active')
 })
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// window.addEventListener("load", () => {
+//   const lazyLoad = document.querySelector(".lazy-load");
+//   lazyLoad.src = lazyLoad.dataset.src;
+//   lazyLoad.addEventListener("load", () =>
+//     lazyLoad.classList.remove("lazy-load")
+//   );
+// });
+
+
