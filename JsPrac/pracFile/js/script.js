9605c383261dfeb27fb5fef832d3dcad1494092a
vimal336 <asvimal336@gmail.com>
2024-04-10 14:42:53 +0530

    tour website using html and css

--- JsPrac/pracFile/js/script.js
+++ JsPrac/pracFile/js/script.js
@@ -2,4 +2,25 @@ console.log('js concepts practice');
 
 
 
+function learnToDebug() 
+{
+    console.log('Learning to debug');
+    return first();
+}
+    
+function first() {
+    console.log('First function');B
+    second();
+    return 100;
+
+}
+    
+function second() {
+console.log('Second function');
+
+}
+
+let result = learnToDebug();
+console.log(result);
+
 
