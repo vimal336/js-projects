9d5f9bbc4dfcd9b24bce01b34ea68e685a8d13bc
vimal336 <asvimal336@gmail.com>
2024-04-15 16:46:55 +0530

    js practice

--- JsPrac/pracFile/js/script.js
+++ JsPrac/pracFile/js/script.js
@@ -182,7 +182,7 @@ console.log(includesMethod);
 // GOOD LUCK 
 
 
-let Arraybills = [125, 555, 44];
+let Arraybills = [100, 555, 44];
 
 let Arraytip = 0;
 
@@ -197,3 +197,37 @@ if (Arraybills[0] >= 50 && Arraybills[0] <= 300) {
 console.log("tip amount: rs" + Arraytip);
 
 console.log("Total amount including tip: rs" + arraytotal);
+
+
+// Coding Challenge #3
+
+// Let's go back to Mark and John comparing their BMIs! This time, let's use objects to 
+// implement the calculations! Remember: BMI = mass / height ** 2 = mass 
+// / (height * height) (mass in kg and height in meter)
+// Your tasks:
+// 1. For each of them, create an object with properties for their full name, mass, and 
+// height (Mark Miller and John Smith)
+
+// 2. Create a 'calcBMI' method on each object to calculate the BMI (the same 
+// method on both objects). Store the BMI value to a property, and also return it from the method
+
+// 3. Log to the console who has the higher BMI, together with the full name and the 
+// respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+// Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+
+
+miller = {
+  fullname : 'Mark Miller',
+  mass : '50',
+  height : '170'
+
+}
+
+smith = {
+  fullname : 'John Smith',
+  mass : '60',
+  height : '160'
+
+}
+
+
