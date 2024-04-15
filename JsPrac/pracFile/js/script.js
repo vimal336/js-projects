50478281e65b53cecf46b4b28d8831c49c77af83
vimal336 <asvimal336@gmail.com>
2024-04-15 17:18:40 +0530

    js practice

--- JsPrac/pracFile/js/script.js
+++ JsPrac/pracFile/js/script.js
@@ -137,14 +137,13 @@ console.log(avgKoalas);
 
 console.log(checkWinner(avgDolhins, avgKoalas));
 
-
-const ArrayMethods = ['one', 'two', 'three', 'four', 7];
+const ArrayMethods = ["one", "two", "three", "four", 7];
 
 // newPush = ArrayMethods.push('five');
 // console.log(ArrayMethods);
 // console.log(newPush);
 
-const unshiftMethod = ArrayMethods.unshift('Zero');
+const unshiftMethod = ArrayMethods.unshift("Zero");
 console.log(unshiftMethod);
 console.log(ArrayMethods);
 
@@ -152,35 +151,33 @@ console.log(ArrayMethods);
 
 //shift removes the first element from an array, while unshift adds elements to the beginning of an array.
 
-//includes method in JavaScript is used to check whether an array includes a certain value among its elements. 
+//includes method in JavaScript is used to check whether an array includes a certain value among its elements.
 //It returns true if the array contains the specified value, and false otherwise.
 
 let includesMethod = ArrayMethods.includes(7);
 console.log(includesMethod);
 
-
 // Coding Challenge #2
 
-// Steven is still building his tip calculator, using the same rules as before: Tip 15% of 
-// the bill if the bill value is between 50 and 300, and if the value is different, the tip is 
+// Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+// the bill if the bill value is between 50 and 300, and if the value is different, the tip is
 // 20%.
 // Your tasks:
-// 1. Write a function 'calcTip' that takes any bill value as an input and returns 
-// the corresponding tip, calculated based on the rules above (you can check out 
-// the code from first tip calculator challenge if you need to). Use the function 
+// 1. Write a function 'calcTip' that takes any bill value as an input and returns
+// the corresponding tip, calculated based on the rules above (you can check out
+// the code from first tip calculator challenge if you need to). Use the function
 // type you like the most. Test the function using a bill value of 100
-// 2. And now let's use arrays! So create an array 'bills' containing the test data 
+// 2. And now let's use arrays! So create an array 'bills' containing the test data
 // below
-// 3. Create an array 'tips' containing the tip value for each bill, calculated from 
+// 3. Create an array 'tips' containing the tip value for each bill, calculated from
 // the function you created before
 // 4. Bonus: Create an array 'total' containing the total values, so the bill + tip
 // Test data: 125, 555 and 44
-// Hint: Remember that an array needs a value in each position, and that value can 
-// actually be the returned value of a function! So you can just call a function as array 
-// values (so don't store the tip values in separate variables first, but right in the new 
-// array) 
-// GOOD LUCK 
-
+// Hint: Remember that an array needs a value in each position, and that value can
+// actually be the returned value of a function! So you can just call a function as array
+// values (so don't store the tip values in separate variables first, but right in the new
+// array)
+// GOOD LUCK
 
 let Arraybills = [100, 555, 44];
 
@@ -198,41 +195,34 @@ console.log("tip amount: rs" + Arraytip);
 
 console.log("Total amount including tip: rs" + arraytotal);
 
-
 // Coding Challenge #3
 
-// Let's go back to Mark and John comparing their BMIs! This time, let's use objects to 
-// implement the calculations! Remember: BMI = mass / height ** 2 = mass 
+// Let's go back to Mark and John comparing their BMIs! This time, let's use objects to
+// implement the calculations! Remember: BMI = mass / height ** 2 = mass
 // / (height * height) (mass in kg and height in meter)
 // Your tasks:
-// 1. For each of them, create an object with properties for their full name, mass, and 
+// 1. For each of them, create an object with properties for their full name, mass, and
 // height (Mark Miller and John Smith)
 
-// 2. Create a 'calcBMI' method on each object to calculate the BMI (the same 
+// 2. Create a 'calcBMI' method on each object to calculate the BMI (the same
 // method on both objects). Store the BMI value to a property, and also return it from the method
 
-// 3. Log to the console who has the higher BMI, together with the full name and the 
+// 3. Log to the console who has the higher BMI, together with the full name and the
 // respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
 // Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
 
-
 miller = {
-  fullname : 'Mark Miller',
-  mass : '50',
-  height : '170'
-
-}
+  fullname: "Mark Miller",
+  mass: "50",
+  height: "170",
+};
 
 smith = {
-  fullname : 'John Smith',
-  mass : '60',
-  height : '160',
-  calcBmi: function() { 
+  fullname: "John Smith",
+  mass: "60",
+  height: "160",
+  calcBmi: function () {
     bmi = weight / (heightInMeters * heightInMeters);
-    return bmi
-}
-
-}
-
-
-
+    return bmi;
+  },
+};
