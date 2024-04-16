74fe2ec621e6f59e3f67586e0cbf0832ce74ae72
vimal336 <asvimal336@gmail.com>
2024-04-16 11:35:25 +0530

    js practice

--- JsPrac/pracFile/js/script.js
+++ JsPrac/pracFile/js/script.js
@@ -255,14 +255,12 @@ for (let i=0; i < phil.length; i++){
 }
 
 
-years = [1999, 1889, 2002, 2010];
+years = [1999, 1889, 2002, 2010, 1999];
 
 const ages = [];
 
 for (let i = 0; i<years.length; i++)
 {
-  ages.push(2037 - years[i]);
+  console.log(ages.push(2037 - years[i]));
 }
-
-console.log(ages);
 
