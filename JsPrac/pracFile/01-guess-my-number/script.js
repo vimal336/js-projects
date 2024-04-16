dc959dac254f998c5956f076fc0cdcb4eaff0aaf
vimal336 <asvimal336@gmail.com>
2024-04-16 16:24:55 +0530

    js practice

--- JsPrac/pracFile/01-guess-my-number/script.js
+++ JsPrac/pracFile/01-guess-my-number/script.js
@@ -1,26 +1,33 @@
 'use strict';
 
 const button = document.querySelector('.check');
-
-
-
-
+const guessInputs = document.querySelector('.guess').value;
+const message = document.querySelector('.message');
+const main = document.querySelector('main');
+let highscore = document.querySelector('.highscore').textContent;
 
 button.addEventListener('click', function() {
   console.log('Button clicked!');
 
-  const guessInputs = document.querySelector('.guess').value;
-  const message = document.querySelector('.message');
 
-let GenerateRandom = Math.floor(Math.random()*20);
-
-console.log(GenerateRandom);
+  let GenerateRandom = Math.floor(Math.random()*20);
+  console.log(GenerateRandom);
 
 if(GenerateRandom == guessInputs){
-   message.innerText="Correct guess"
-} else{
-    console.log("your guessed is wrong")
+   message.innerText="Correct guess";
+   main.style.backgroundColor ="green";
+   highscore =`${guessInputs}`;
+   
+}else if(GenerateRandom < guessInputs){
+    message.innerText="too low";
+    main.style.backgroundColor ="#c600d5";
+    highscore =`${guessInputs}`;
 }
 
+else if(GenerateRandom > guessInputs){
+    message.innerText="too high";
+    main.style.backgroundColor ="blue";
+    highscore =`${guessInputs}`;
+}
 
 });
