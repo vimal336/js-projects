e92f8c44061a9485781bd43bda07633fe2a17569
vimal336 <asvimal336@gmail.com>
2024-04-16 12:49:19 +0530

    js practice

--- JsPrac/pracFile/01-guess-my-number/script.js
+++ JsPrac/pracFile/01-guess-my-number/script.js
@@ -1,6 +1,10 @@
 'use strict';
 
+let guess = document.querySelectorAll('guess');
+console.log(guess);
+
 let GenerateRandom = Math.floor(Math.random()*20);
 
 console.log(GenerateRandom);
 
+if(GenerateRandom == )
