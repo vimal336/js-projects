81ccc94d81fe52924e12a8e700385ad31b8c5b31
vimal336 <asvimal336@gmail.com>
2024-04-16 15:08:58 +0530

    js practice

--- JsPrac/pracFile/01-guess-my-number/script.js
+++ JsPrac/pracFile/01-guess-my-number/script.js
@@ -9,15 +9,15 @@ const button = document.querySelector('.check');
 button.addEventListener('click', function() {
   console.log('Button clicked!');
 
-  const guessInputs = document.querySelector('guess').value;
-  console.log(guessInputs);
+  const guessInputs = document.querySelector('.guess').value;
+  const message = document.querySelector('.message');
 
 let GenerateRandom = Math.floor(Math.random()*20);
 
 console.log(GenerateRandom);
 
 if(GenerateRandom == guessInputs){
-    console.log("your guessed is right")
+   message.innerText="Correct guess"
 } else{
     console.log("your guessed is wrong")
 }
