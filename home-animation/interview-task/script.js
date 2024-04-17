bb2b69e7ad69414e51b7356dcb4ce1f1c254504d
vimal336 <asvimal336@gmail.com>
2024-04-17 16:40:59 +0530

    js practice

--- home-animation/interview-task/script.js
+++ home-animation/interview-task/script.js
@@ -9,3 +9,18 @@ function activeClass() {
   menuBtn.classList.toggle("active");
   menu.classList.toggle("active");
 }
+
+const moveTop = document.querySelector('.top');
+ 
+window.addEventListener('scroll', () => {
+
+var y = window.scrollY;
+
+if(y >= 700){
+  moveTop.classList.add('Topactive');
+} else{
+  moveTop.classList.remove('Topactive');
+}
+
+
+});
