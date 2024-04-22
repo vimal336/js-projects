4f67af432d706b13d72762f763504e89e21f8dd3
vimal336 <asvimal336@gmail.com>
2024-04-22 12:18:56 +0530

    js practice

--- JsPrac/pracFile/01-guess-my-number/script.js
+++ JsPrac/pracFile/01-guess-my-number/script.js
@@ -4,45 +4,37 @@ const button = document.querySelector('.check');
 const message = document.querySelector('.message');
 const main = document.querySelector('main');
 let highscore = document.querySelector('.highscore');
-let score = document.querySelector('.score');
+let score = 20;
 
-button.addEventListener('click', function() {
-
+button.addEventListener('click', function () {
   let guessInputs = Number(document.querySelector('.guess').value);
   console.log(typeof guessInputs);
   console.log(highscore);
 
-if (!guessInputs) {
-message.textContent = 'No number';
-return;
-}
+  if (!guessInputs) {
+    message.textContent = 'No number';
+    return;
+  }
 
-let score = 20;
+  let GenerateRandom = Math.trunc(Math.random() * 20);
 
-score--;
+  document.querySelector('.number').textContent = GenerateRandom;
 
-score.textContent = score;
-
-
-let GenerateRandom = Math.trunc(Math.random()*20);
-
-document.querySelector('.number').textContent = GenerateRandom;
-
-if(GenerateRandom == guessInputs){
-   message.innerText="Correct guess";
-   main.style.backgroundColor ="green";
-   highscore.innerText = `${guessInputs}`; 
-   
-}else if(GenerateRandom < guessInputs){
-    message.innerText="too low";
-    main.style.backgroundColor ="orange";
-    highscore.innerText = `${guessInputs}`; 
-}
-
-else if(GenerateRandom > guessInputs){
-    message.innerText="too high";
-    main.style.backgroundColor ="red";
-    highscore.innerText = `${guessInputs}`; 
-}
-
+  if (GenerateRandom == guessInputs) {
+    message.innerText = 'Correct guess';
+    main.style.backgroundColor = 'green';
+    highscore.innerText = `${guessInputs}`;
+  } else if (GenerateRandom < guessInputs) {
+    message.innerText = 'too low';
+    main.style.backgroundColor = 'orange';
+    highscore.innerText = `${guessInputs}`;
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (GenerateRandom > guessInputs) {
+    message.innerText = 'too high';
+    main.style.backgroundColor = 'red';
+    highscore.innerText = `${guessInputs}`;
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
 });
