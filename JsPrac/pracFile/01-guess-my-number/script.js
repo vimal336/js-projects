85241d40ee94f7bde1754acf3b988f42c15bf6b0
vimal336 <asvimal336@gmail.com>
2024-04-22 11:46:43 +0530

    js practice

--- JsPrac/pracFile/01-guess-my-number/script.js
+++ JsPrac/pracFile/01-guess-my-number/script.js
@@ -19,22 +19,23 @@ return;
 
 let GenerateRandom = Math.trunc(Math.random()*20);
 
-let score = 20;
-
 document.querySelector('.number').textContent = GenerateRandom;
 
 if(GenerateRandom == guessInputs){
    message.innerText="Correct guess";
    main.style.backgroundColor ="green";
+   highscore.innerText = `${guessInputs}`; 
    
 }else if(GenerateRandom < guessInputs){
     message.innerText="too low";
     main.style.backgroundColor ="orange";
+    highscore.innerText = `${guessInputs}`; 
 }
 
 else if(GenerateRandom > guessInputs){
     message.innerText="too high";
     main.style.backgroundColor ="red";
+    highscore.innerText = `${guessInputs}`; 
 }
 
 });
