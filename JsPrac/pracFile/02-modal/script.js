9b86e80ce93fa3e8d331c23b5627d8add7606263
vimal336 <asvimal336@gmail.com>
2024-04-23 13:38:12 +0530

    js practice

--- JsPrac/pracFile/02-modal/script.js
+++ JsPrac/pracFile/02-modal/script.js
@@ -5,24 +5,29 @@ let modal = document.querySelector('.modal');
 let overlay = document.querySelector('.overlay');
 let closeModal = document.querySelector('.close-modal');
 
-modalButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    });
+modalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
 });
 
+const commonOpenModals = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  };
+  
 
+const commonCloseModals = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
-    closeModal.addEventListener('click', () => {
-        modal.classList.add('hidden');
-        overlay.classList.add('hidden');
-    });
-
+closeModal.addEventListener('click', commonCloseModals);
+overlay.addEventListener('click', commonCloseModals);
 
 
 // for(let i=0; i<=modalButtons.length; i++)
 //   modalButtons[i].addEventListener('click', function(){
 //   modal.classList.remove('hidden');
 // });
-  
