977b6f160068c4f7e26b5d584efe63331e3a62c8
vimal336 <asvimal336@gmail.com>
2024-04-23 10:37:14 +0530

    js practice

--- JsPrac/pracFile/02-modal/script.js
+++ JsPrac/pracFile/02-modal/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+let modalButtons = document.querySelectorAll('.show-modal');
+
+modalButtons.forEach((modals) => {
+    modals.addEventListener('click', () => {
+        modals.classList.remove('hidden');
+    });
+  });
