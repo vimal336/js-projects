6cce450462ef9376ca294fed09246192706034e6
vimal336 <asvimal336@gmail.com>
2024-04-23 12:22:25 +0530

    js practice

--- JsPrac/pracFile/02-modal/script.js
+++ JsPrac/pracFile/02-modal/script.js
@@ -1,17 +1,18 @@
 'use strict';
 
 let modalButtons = document.querySelectorAll('.show-modal');
-let modal = document.querySelectorAll('.modal');
+let modal = document.querySelector('.modal');
 
 modalButtons.forEach((button) => {
-      console.log(button.textContent);
-  });
+    button.addEventListener('click', () => {
+        modal.classList.remove('hidden');
+    });
+});
 
 
-for(let i=0; i<=modalButtons.length; i++){
-  modalButtons[i].addEventListener('click', function(){
 
-  console.log('clicked')
-})
-};
+// for(let i=0; i<=modalButtons.length; i++)
+//   modalButtons[i].addEventListener('click', function(){
+//   modal.classList.remove('hidden');
+// });
   
