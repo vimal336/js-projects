948329a9f0e53b66580ed452e660bc392014fe29
vimal336 <asvimal336@gmail.com>
2024-04-23 17:39:00 +0530

    js practice

--- JsPrac/pracFile/03-Pig-Game/script.js
+++ JsPrac/pracFile/03-Pig-Game/script.js
@@ -3,4 +3,6 @@
 //Selecting Elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-console.log(score0El);
+
+score0El.textContent = 0;
+score0El.textContent = 1;
