fe94923722bf583e6fb9f6d6d83f8e6a5008eee5
vimal336 <asvimal336@gmail.com>
2024-04-23 12:54:20 +0530

    js practice

--- JsPrac/pracFile/02-modal/script.js
+++ JsPrac/pracFile/02-modal/script.js
@@ -2,21 +2,22 @@
 
 let modalButtons = document.querySelectorAll('.show-modal');
 let modal = document.querySelector('.modal');
+let overlay = document.querySelector('.overlay');
 let closeModal = document.querySelector('.close-modal');
 
 modalButtons.forEach((button) => {
     button.addEventListener('click', () => {
         modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
     });
 });
 
 
-modalButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        closeModal.classList.add('hidden');
-    });
-});
 
+    closeModal.addEventListener('click', () => {
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    });
 
 
 
