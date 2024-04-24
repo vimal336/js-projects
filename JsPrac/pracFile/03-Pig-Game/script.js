5b7900779cab7cf14ac5d2b04f57b84bc861a7fc
vimal336 <asvimal336@gmail.com>
2024-04-24 11:24:04 +0530

    js practice

--- JsPrac/pracFile/03-Pig-Game/script.js
+++ JsPrac/pracFile/03-Pig-Game/script.js
@@ -1,11 +1,11 @@
 'use strict';
 
-//Selecting Elements
+
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--role');
+const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 
@@ -23,19 +23,7 @@ console.log(randomDice);
 
 diceEl.classList.remove('hidden');
 
-if (randomDice === 1) {
-    diceEl.src = "dice-1.png";
-} else if (randomDice === 2) {
-    diceEl.src = "dice-2.png";
-} else if (randomDice === 3) {
-    diceEl.src = "dice-3.png";
-} else if (randomDice === 4) {
-    diceEl.src = "dice-4.png";
-} else if (randomDice === 5) {
-    diceEl.src = "dice-5.png";
-} else {
-    diceEl.src = "dice-6.png";
-}
-
+diceEl.src = "dice-1.png";
 
 });
+
