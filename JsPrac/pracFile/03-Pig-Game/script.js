91684aea35d2d2bac7aed3abcc7a4d908f386e14
vimal336 <asvimal336@gmail.com>
2024-04-24 12:05:14 +0530

    js practice

--- JsPrac/pracFile/03-Pig-Game/script.js
+++ JsPrac/pracFile/03-Pig-Game/script.js
@@ -17,6 +17,8 @@ score1El.textContent = 0;
 
 diceEl.classList.add('hidden');
 
+currentScore = 0;
+
 //Rolling Dice Functionality
 btnRoll.addEventListener('click', function(){
 
@@ -25,8 +27,6 @@ let randomDice = Math.trunc(Math.random()*6) + 1;
 diceEl.classList.remove('hidden');
 
 diceEl.src = `dice-${randomDice}.png`;
-
-
 
 });
 
