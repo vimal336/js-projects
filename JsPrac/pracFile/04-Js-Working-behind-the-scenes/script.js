af81638f999a0306384a14d155c5b6a7bbb5ca3e
vimal336 <asvimal336@gmail.com>
2024-04-26 12:02:09 +0530

    js practice

--- JsPrac/pracFile/04-Js-Working-behind-the-scenes/script.js
+++ JsPrac/pracFile/04-Js-Working-behind-the-scenes/script.js
@@ -3,3 +3,11 @@
 
 var foo = 'fo';
 console.log(fo);
+
+
+// let example
+let b = 10;
+if (true) {
+    let b = 20; // This declares a new 'b' in a different block scope
+}
+console.log(b); // Output: 10
