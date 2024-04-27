6d92a51ff26dedbe49b4535638e4a3e423973c68
vimal336 <asvimal336@gmail.com>
2024-04-27 22:16:11 +0530

    js practice

---
+++ JsPrac/pracFile/JsConcepts/execution-context/execution.js
@@ -0,0 +1,20 @@
+
+// How JavaScript works?
+// Ans-Everything in JavaScript happens inside an "execution context".
+
+// Execution context has two component
+
+// 1)memory component[variable environment] 
+
+// -This is the place where all variables and functions are stored as key value pairs. eg-{key: value ||  n:2;} 
+
+//  2)code component[Thread of execution]
+
+// -This is the place where code is executed one line at a time
+
+// Note-
+
+// *JavaScript is a synchronous single-threaded language
+
+// -Single threaded means JavaScript can execute once command at a time
+// -Synchronous single-threaded that means JavaScript can execute one command at a time in a specific order.
--- JsPrac/pracFile/JsConcepts/this-keyword/this.js
+++ JsPrac/pracFile/JsConcepts/this-keyword/this.js
@@ -4,7 +4,20 @@
 // It is determined dynamically and can vary depending on how a function is called.
 
 
+console.log(this);//in normal it refers window object
 
+console.log(this);//in strict mode its undefined
+
+console.log(this);//in arrow function this keyword refers window object
+
+//arrow function does not get its own this, but inside regular func this works
+
+
+const fun = () =>{
+    console.log("function")
+}
+
+fun();
 
 
 
