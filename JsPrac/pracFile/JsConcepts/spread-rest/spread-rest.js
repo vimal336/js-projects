c4901631ebaf3b47686b0b70cd6d10c3574de21f
vimal336 <asvimal336@gmail.com>
2024-04-29 14:56:41 +0530

    js practice

--- JsPrac/pracFile/JsConcepts/spread-rest/spread-rest.js
+++ JsPrac/pracFile/JsConcepts/spread-rest/spread-rest.js
@@ -1,5 +1,25 @@
 //Spread Operator
+
+// the spread operator spreads (or expands) an array into individual elements. 
+// It was also introduced in ECMAScript 2015 (ES6) and is used in various contexts, 
+// such as function calls, array literals, and object literals.
+
+
 const numbersOne = [1, 2, 3];
 const numbersTwo = [4, 5, 6];
 const numbersCombined = [...numbersOne, ...numbersTwo, '7'];
 console.log(numbersCombined);
+
+
+//Rest operator
+
+// The rest parameter syntax in JavaScript allows functions to accept an indefinite number of arguments as an array. 
+// It's indicated by three dots (...) preceding the last parameter of a function.
+// This feature was introduced in ECMAScript 2015 (ES6).
+
+
+function sum(...numbers) {
+    return numbers.reduce((acc, num) => acc + num, 0);
+}
+
+console.log(sum(1, 2, 3, 4)); // Output: 10
---
+++ JsPrac/pracFile/JsConcepts/truthy-falsy/truthy-falsy.js
@@ -0,0 +1,30 @@
+// In JavaScript, values are inherently truthy or falsy when evaluated in a boolean context. 
+
+//Understanding truthy and falsy values is important for writing conditional logic in JavaScript.
+
+
+// Truthy values: Values that are considered true when evaluated in a boolean context. 
+//Any value that is not falsy is truthy.
+
+// Examples of truthy values include:
+//-----------------------------------//
+
+// Non-empty strings ("hello")
+// Non-zero numbers (except for 0)
+// Objects
+// Arrays
+// Functions
+// true
+// new Date()
+// Falsy values: Values that are considered false when evaluated in a boolean context.
+
+
+// Examples of falsy values include:
+//---------------------------------------//
+
+// Empty strings ("")
+// 0 (zero)
+// NaN (Not a Number)
+// null
+// undefined
+// false
