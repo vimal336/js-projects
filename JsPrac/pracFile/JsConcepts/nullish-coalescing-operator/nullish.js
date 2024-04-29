a73722070335fc52d255d5fe6253e5de84549ce7
vimal336 <asvimal336@gmail.com>
2024-04-29 10:16:53 +0530

    js practice

--- JsPrac/pracFile/JsConcepts/nullish-coalescing-operator/nullish.js
+++ JsPrac/pracFile/JsConcepts/nullish-coalescing-operator/nullish.js
@@ -1,3 +1,12 @@
 // The nullish coalescing (??) operator is a logical operator that returns its right-hand side
 //  operand when its left-hand side operand is null or undefined,
 //  and otherwise returns its left-hand side operand.
+
+
+const foo = null ?? 'default string';
+console.log(foo);
+// Expected output: "default string"
+
+const baz = 0 ?? 42;
+console.log(baz);
+// Expected output: 0
