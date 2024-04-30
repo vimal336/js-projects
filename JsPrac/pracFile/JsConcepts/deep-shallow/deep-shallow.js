f8f7106e099a881ebc0a0da392dc8bac84a52e1b
vimal336 <asvimal336@gmail.com>
2024-04-30 12:49:20 +0530

    js practice

--- JsPrac/pracFile/JsConcepts/deep-shallow/deep-shallow.js
+++ JsPrac/pracFile/JsConcepts/deep-shallow/deep-shallow.js
@@ -19,3 +19,12 @@ console.log(copiedString);   // Output: Hello (copiedString remains unchanged)
 // meaning their values can be changed. When you assign an array or an object to another variable, 
 // you're creating a reference to the original array or object, not a copy of it. This means that changes
 // made to the original array or object will affect the copy and vice versa. This is known as a shallow copy.
+
+let originalArray = [1, 2, 3];
+let copiedArray = originalArray.slice();
+
+// Now let's change the value of an element in originalArray
+originalArray[0] = 100;
+
+console.log(originalArray); // Output: [100, 2, 3]
+console.log(copiedArray);   // Output: [1, 2, 3] (copiedArray remains unchanged)
