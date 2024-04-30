9bcebdf82285a5db67714521e89399070443f888
vimal336 <asvimal336@gmail.com>
2024-04-30 16:56:22 +0530

    js practice

--- JsPrac/pracFile/JsConcepts/arrray-methods/slice.js
+++ JsPrac/pracFile/JsConcepts/arrray-methods/slice.js
@@ -2,3 +2,20 @@
 // and return it as a new array. 
 // It doesn't modify the original array; instead, 
 // it returns a shallow copy of a portion of the array specified by its parameters. 
+
+array.slice(startIndex, endIndex)
+
+
+const fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry'];
+
+// Extract a portion of the array
+const slicedFruits = fruits.slice(1, 4); // Returns ['banana', 'cherry', 'date']
+
+// Slice from a negative index
+const slicedFruitsFromEnd = fruits.slice(-3); // Returns ['cherry', 'date', 'elderberry']
+
+// Slice without endIndex (extracts till the end)
+const slicedFruitsTillEnd = fruits.slice(2); // Returns ['cherry', 'date', 'elderberry']
+
+// Slice with negative endIndex
+const slicedFruitsNegativeEndIndex = fruits.slice(1, -1); // Returns ['banana', 'cherry', 'date']
