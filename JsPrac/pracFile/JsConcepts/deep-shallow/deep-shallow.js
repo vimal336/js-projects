b39640ae290af0c1452b1924ff925778f6be640f
vimal336 <asvimal336@gmail.com>
2024-04-30 12:45:34 +0530

    js practice

--- JsPrac/pracFile/JsConcepts/deep-shallow/deep-shallow.js
+++ JsPrac/pracFile/JsConcepts/deep-shallow/deep-shallow.js
@@ -1,2 +1,13 @@
 // In JavaScript, primitive data types like strings, numbers, 
 // and booleans are immutable, meaning their values cannot be changed. 
+
+let originalString = "Hello";
+let copiedString = originalString;
+
+console.log(copiedString); // Output: Hello
+
+// Now let's change the value of originalString
+originalString = "Hi";
+
+console.log(originalString); // Output: Hi
+console.log(copiedString);   // Output: Hello (copiedString remains unchanged)
