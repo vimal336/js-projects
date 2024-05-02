d870b0bbe97810ef3993514209c81ce631fdc209
vimal336 <asvimal336@gmail.com>
2024-05-02 16:56:31 +0530

    js practice

--- JsPrac/pracFile/JsConcepts/closure/closure.js
+++ JsPrac/pracFile/JsConcepts/closure/closure.js
@@ -12,4 +12,32 @@ function init() {
     displayName();
   }
   init();
+
+//returning functions - higher order function
+
+//lexical scoping - inner scope can access parent scope variables
+
+//A closure is the combination of a function bundled together (enclosed) 
+//with references to its surrounding state(the lexical environment). 
+//In other words, a closure gives you access to an outer function's scope from an inner function.
+
+
+function outer(name){
+    let outerVariable = 'Bread'
+    function inner(){
+        let innerVariable = 'Butter'
+        console.log('inner variable',innerVariable)
+        console.log('outer variable',outerVariable)
+        console.log('a is',a)
+        console.log('hello',name)
+    }
+    return inner
+}   
+
+let call1 = outer('Vidya')
+call1()
+
+let call2 = outer('John')
+call2()
+call1()
   
