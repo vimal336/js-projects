b0a97249db87553001e824a9c925ecaa274d2db0
vimal336 <asvimal336@gmail.com>
2024-05-02 16:00:06 +0530

    js practice

---
+++ JsPrac/pracFile/JsConcepts/closure/closure.js
@@ -0,0 +1,3 @@
+// JavaScript closure is a feature that allows inner functions to access the outer scope of a function. 
+// Closure helps in binding a function to its outer boundary and is created automatically 
+// whenever a function is created. 
