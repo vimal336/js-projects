c17c22082ad77e4907db4014ba309fa7840e75d5
vimal336 <asvimal336@gmail.com>
2024-05-02 14:41:02 +0530

    js concepts Practice

--- JsPrac/pracFile/JsConcepts/bind-call-apply/bind-call-apply.js
+++ JsPrac/pracFile/JsConcepts/bind-call-apply/bind-call-apply.js
@@ -1,10 +1,6 @@
-
-
 //Call Method
 
 // call method: It invokes a function with a specified this value and arguments provided individually.
-
-
 
 function sayHi(a, b) {
     console.log('Hi, ' + this.name, a + b);
@@ -17,17 +13,25 @@ var person = {
 sayHi.call(person, 5, 6); // Output: Hello, Vimal 11
 
 
+//Apply Method
 
+//apply method: It is similar to call, but it accepts arguments as an array.
 
+function sayHello(greeting) {
+    console.log(greeting + ', ' + this.name);
+}
 
+var person = {
+    name: 'Bob'
+};
 
-
+sayHello.apply(person, ['Hola']); // Output: Hola, Bob
 
 //Bind Method
 
 // bind method: It returns a new function with the this value set to the provided value. 
 // Unlike call and apply, it doesn't immediately invoke the function;
-//  instead, it creates a new function with the specified context.
+// instead, it creates a new function with the specified context.
 
 //Example -1
 
