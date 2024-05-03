1f21629bc967fce56c7581151c3ba387f7c39754
vimal336 <asvimal336@gmail.com>
2024-05-03 16:27:13 +0530

    js practice

--- JsPrac/pracFile/06-Project-Bankist/script.js
+++ JsPrac/pracFile/06-Project-Bankist/script.js
@@ -68,9 +68,9 @@ movements.forEach(function (mov,i){
 
   const html = `
   <div class ="movements__row">
-  <div class = "movements__type movements__type--deposit>${i+1} ${type}
+  <div class ="movements__type movements__type--${type}">${i + 1} ${type}
   </div>
-  <div class="movements__value">${mov}<div>
+  <div class="movements__value">${mov}€<div>
   </div>
   `;
 
