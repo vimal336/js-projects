1b9ab9cd30fb3d5793a8de8a2c72f0682b8dbaf0
vimal336 <asvimal336@gmail.com>
2024-05-03 16:10:56 +0530

    js practice

--- JsPrac/pracFile/06-Project-Bankist/script.js
+++ JsPrac/pracFile/06-Project-Bankist/script.js
@@ -67,6 +67,7 @@ movements.forEach(function (mov,i){
   const type = mov > 0 ? 'deposit' : 'withdrawl';
 
   const html = `
+  <div class ="movements__row">
   <div class = "movements__type movements__type--deposit>${i+1} ${type}
   </div>
   <div class="movements__value">${mov}<div>
