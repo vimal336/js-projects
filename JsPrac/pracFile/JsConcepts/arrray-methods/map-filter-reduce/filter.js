e2bc99f6595f05daa919000e70b9a2ada48cf150
vimal336 <asvimal336@gmail.com>
2024-05-06 12:24:23 +0530

    js practice

--- JsPrac/pracFile/JsConcepts/arrray-methods/map-filter-reduce/filter.js
+++ JsPrac/pracFile/JsConcepts/arrray-methods/map-filter-reduce/filter.js
@@ -3,3 +3,14 @@
 
 // The filter method doesn't modify the original array, but instead creates and returns 
 // a new array containing only the elements that meet the specified condition.
+
+
+
+const numbers = [1, 2, 3, 4, 5];
+const evenNumbers = numbers.filter(num => num % 2 === 0);
+// evenNumbers: [2, 4]
+
+
+const filternum = numbers.filter((fil))
+
+console.log(filt);
