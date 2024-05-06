9e1ad67a3d402e151dfcd3196f48f7525ddb1551
vimal336 <asvimal336@gmail.com>
2024-05-06 15:09:05 +0530

    js practice

--- JsPrac/pracFile/JsConcepts/bind-call-apply/bind-call-apply.js
+++ JsPrac/pracFile/JsConcepts/bind-call-apply/bind-call-apply.js
@@ -3,67 +3,95 @@
 // call method: It invokes a function with a specified this value and arguments provided individually.
 
 function sayHi(a, b) {
-    console.log('Hi, ' + this.name, a + b);
+  console.log("Hi, " + this.name, a + b);
 }
 
 var person = {
-    name: 'Vimal'
+  name: "Vimal",
 };
 
 sayHi.call(person, 5, 6); // Output: Hello, Vimal 11
-
 
 //Apply Method
 
 //apply method: It is similar to call, but it accepts arguments as an array.
 
 function sayHello(greeting) {
-    console.log(greeting + ', ' + this.name);
+  console.log(greeting + ", " + this.name);
 }
 
 var person = {
-    name: 'Bob'
+  name: "Bob",
 };
 
-sayHello.apply(person, ['Hola']); // Output: Hola, Bob
+sayHello.apply(person, ["Hola"]); // Output: Hola, Bob
 
 //Bind Method
 
-// bind method: It returns a new function with the this value set to the provided value. 
+// bind method: It returns a new function with the this value set to the provided value.
 // Unlike call and apply, it doesn't immediately invoke the function;
 // instead, it creates a new function with the specified context.
 
 //Example -1
 
-function myfun(){
-    console.log(this);
+function myfun() {
+  console.log(this);
 }
 
 let obj = {
-    name:'joining',
-    concept:'bind method'
+  name: "joining",
+  concept: "bind method",
 };
 
 myfun = myfun.bind(obj);
 myfun();
 
-
 //Example - 2
 
-function sayHello() {
-    console.log('Hello, ' + this.name);
-    
+function saygreet() {
+  console.log("Hello, " + this.name);
 }
 
-var person = {
-    name: 'Charlie',
-    age:'20'
+var people = {
+  name: "Charlie",
+  age: "20",
 };
 
+var people2 = {
+  name: "Charlie",
+  age: "20",
+};
 
-var greetPerson = sayHello.bind(person);
+var greetPerson = saygreet.bind(people);
 
-var greetPerson2 = sayHello.bind(person);
+var greetPerson2 = saygreet.bind(people2);
 
 greetPerson2();
 greetPerson(); // Output: Hello, Charlie
+
+
+
+function out() {
+  let a = 23;
+
+  function inner() {
+    let c = a + 20;
+    return c;
+  }
+
+  inner();
+}
+
+out();
+
+
+const numbers = [1, 2, 3];
+
+function double() {
+  return this * 2;
+}
+
+const doubledNumbers = numbers.map(double, 3); 
+
+console.log(doubledNumbers); 
+
