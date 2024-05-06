d1690ffdbb0bdad62bc00743f2d46fb6a410aa64
vimal336 <asvimal336@gmail.com>
2024-05-06 11:16:34 +0530

    js practice

--- JsPrac/pracFile/JsConcepts/arrray-methods/map-filter-reduce/reduce.js
+++ JsPrac/pracFile/JsConcepts/arrray-methods/map-filter-reduce/reduce.js
@@ -16,17 +16,30 @@ const reducearray = reducemethod.reduce((prev, curr, i, arr)=>{
 });
 
 
+// A callback function: This function is executed for each element in the array. 
+// It can take four arguments:
+// Accumulator: The accumulated result of previous iterations. 
+// It is the value returned by the previous invocation of the callback, or the initial value if provided.
+
+// Current value: The current element being processed in the array.
+// Current index: The index of the current element being processed.
+// Source array: The array reduce() was called upon.
+// Initial value (optional): An optional initial value to use as the first argument to the callback function. 
+// If not provided, the first element of the array will be used as the initial accumulator value, 
+// and iteration will start from the second element.
+
 
 const numbers = [1, 2, 3, 4, 5];
 
 const sum = numbers.reduce((accumulator, currentValue) => {
   return accumulator + currentValue;
-}, 0); // Initial value of accumulator is 0
+}, 1); // Initial value of accumulator is 1
 
-console.log(sum); // Output: 15 (1 + 2 + 3 + 4 + 5)
-
-
+console.log(sum); // Output: 16 (1 + 2 + 3 + 4 + 5)
 
 
+
+// the reduce() method to perform various operations like summing, averaging, filtering, 
+// or any custom operation that requires reducing an array to a single value.
 
 
