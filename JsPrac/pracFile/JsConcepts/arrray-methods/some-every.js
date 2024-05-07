90daa96c3a9fcb31e608fccf92ab1dd4d807b7d6
vimal336 <asvimal336@gmail.com>
2024-05-07 12:29:07 +0530

    js practice

--- JsPrac/pracFile/JsConcepts/arrray-methods/some-every.js
+++ JsPrac/pracFile/JsConcepts/arrray-methods/some-every.js
@@ -1,5 +1,13 @@
+// The some() method tests whether at least one element in the array 
+// passes the test implemented by the provided function. 
+// It returns true if at least one element in the array satisfies the condition, otherwise, it returns false.
+
+
+
 const numbers = [1, 2, 3, 4, 5];
 
 const hasEven = numbers.some(element => element % 2 === 0);
 
 console.log(hasEven); // Output: true (because at least one element in the array is even)
+
+
