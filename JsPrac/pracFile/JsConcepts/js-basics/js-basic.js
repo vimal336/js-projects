005adb5347315cccc6333da64cb38fe2c008264f
vimal336 <asvimal336@gmail.com>
2024-05-07 17:54:35 +0530

    js practice

---
+++ JsPrac/pracFile/JsConcepts/js-basics/js-basic.js
@@ -0,0 +1,52 @@
+
+// JavaScript has several data types, which can be categorized into two main 
+
+// categories: primitive data types and reference data types.
+
+
+// JavaScript is a dynamically typed language, meaning variables can hold values of any data type, 
+// and data types are automatically converted as needed during operations. 
+// Understanding these data types and how they behave is fundamental to writing effective JavaScript code.
+
+//Javascript operators
+
+//Arithmetic Operators
+
+let a = 5;
+let b = 2;
+
+let sum = a + b;      // Addition
+let difference = a - b; // Subtraction
+let product = a * b;  // Multiplication
+let quotient = a / b; // Division
+let remainder = a % b; // Modulus
+
+console.log(sum);        // Output: 7
+console.log(difference); // Output: 3
+console.log(product);    // Output: 10
+console.log(quotient);   // Output: 2.5
+console.log(remainder);  // Output: 1
+
+//Assignment Operators
+
+let x = 10;
+
+x += 5; // Equivalent to: x = x + 5;
+console.log(x); // Output: 15
+
+x *= 2; // Equivalent to: x = x * 2;
+console.log(x); // Output: 30
+
+
+x **= 2;
+
+console.log(x);
+
+//comparison operator
+
+let num1 = 10;
+let num2 = 5;
+
+console.log(num1 > num2);  // Output: true
+console.log(num1 === num2); // Output: false
+
