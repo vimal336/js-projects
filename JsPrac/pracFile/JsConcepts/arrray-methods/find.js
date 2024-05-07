fcb36d12bbdba962675695ebc32cbf1829a70064
vimal336 <asvimal336@gmail.com>
2024-05-07 11:44:41 +0530

    js practice

--- JsPrac/pracFile/JsConcepts/arrray-methods/find.js
+++ JsPrac/pracFile/JsConcepts/arrray-methods/find.js
@@ -1,5 +1,7 @@
 
+//find() is used to retrieve the first element in an array Otherwise, it returns undefined.
 
+// find gives a value type but filter gives a array type.
 
 const numbers = [1, 2, 3, 4, 5];
 const found = numbers.find(element => element > 3);
