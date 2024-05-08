136ec9027a66e6ba77c4f98a31d2b6bd2fa6f355
vimal336 <asvimal336@gmail.com>
2024-05-08 10:51:55 +0530

    js practice

--- JsPrac/pracFile/JsConcepts/js-basics/js-basic.js
+++ JsPrac/pracFile/JsConcepts/js-basics/js-basic.js
@@ -50,3 +50,101 @@ let num2 = 5;
 console.log(num1 > num2);  // Output: true
 console.log(num1 === num2); // Output: false
 
+console.log(num1 != num2);
+
+console.log(num1 !== num2)
+
+
+//logical operators
+
+let isSunny = 0;
+let isWarm = 10;
+
+console.log(isSunny && isWarm); // Output: true
+console.log(isSunny || isWarm); // Output: true
+console.log(!isSunny);          // Output: false
+
+
+//ternary operator
+
+let msg = num1>=19 ? "a" : "b"
+
+console.log(msg);
+
+
+//if else
+
+if(num1>=19){
+ console.log('a')
+} else{
+    console.log('b') 
+}
+
+
+let array = ['1', '2', '4', '5']
+
+let maps  =  array.map((i)=>{
+    console.log(i);
+});
+
+
+// let filt = array.filter((fill)=>{
+//   let va= fill >=2;
+//    console.log(va);
+// });
+// console.log(filt)
+
+
+
+let filt = array.filter((fill) => {
+  let va = fill >= 2;
+  return va; // Return the result of the condition
+});
+
+console.log(filt);
+
+
+
+
+function fun(a, b){
+   c = a+b;
+   return c
+}
+fun(5,5);
+
+
+
+//iife - immediately invoked function expression
+
+(function iif(){
+  console.log("iife")
+})();
+
+
+//nan
+
+let notanum = 4;
+
+let fruit = "apple"
+
+nanex = notanum*fruit;
+
+console.log(nanex);
+
+//type casting
+
+let num3 = 5;
+
+let num4 = "23";
+
+num = Number(num4);
+
+num5 = num3 + num;
+
+console.log(num5);
+
+//Type coercion, also known as implicit type conversion, is the process of automatically 
+//converting values from one data type to another during operations or comparisons.
+
+console.log(5 + true); // Output: 6 (true is coerced to 1)
+console.log(10 - false); // Output: 10 (false is coerced to 0)
