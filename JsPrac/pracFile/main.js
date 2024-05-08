3d5b9b3722c302666d09f4eb87c440063ca9c4ed
vimal336 <asvimal336@gmail.com>
2024-05-08 11:26:30 +0530

    js practice

--- JsPrac/pracFile/main.js
+++ JsPrac/pracFile/main.js
@@ -1,1 +1,16 @@
 
+// let head = document.querySelector('.head');
+// console.log(head);
+
+
+// let arr = [1,2,3,4,5];
+
+// for(let i=arr.length; i>=0; i--){
+//   console.log(arr[i]);
+// }
+
+let arr = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(i);
+}
