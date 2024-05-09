15ffe84b07965e2baba8bd3d9ee33327adc106e2
vimal336 <asvimal336@gmail.com>
2024-05-09 10:31:12 +0530

    js practice

--- JsPrac/pracFile/main.js
+++ JsPrac/pracFile/main.js
@@ -1,6 +1,16 @@
 
-// let head = document.querySelector('.head');
-// console.log(head);
+// // Get a reference to the button element
+// let button = document.querySelector(".myButton");
+
+
+// function handleClick() {
+//     console.log("Button clicked!");
+// }
+
+
+// button.addEventListener("click", handleClick);
+
+
 
 
 // let arr = [1,2,3,4,5];
@@ -20,6 +30,158 @@ let  rev = arr.reverse();
 
 console.log(rev);
 
-setTimeout(function(){
-    console.log("set interval");
-},5000)
+// setTimeout(function(){
+//     console.log("set interval");
+// },5000)
+
+i = 0;
+
+while(i<10){
+  i++;
+  console.log(1);
+}
+
+j = 0;
+
+do {
+  console.log(j);
+  j++;
+} while (j < 5);
+
+
+var matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+for (var i = 0; i < matrix.length; i++) {
+    for (var j = 0; j < matrix[i].length; j++) {
+        console.log(matrix[i][j]);
+    }
+}
+
+
+// for of
+
+for(let forof of arr){
+    console.log(forof);
+}
+
+
+
+//for in
+
+var person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+
+
+
+for (var key in person) {
+    console.log(key + ": " + person[key]);
+}
+
+// for each
+
+let forech = arr.forEach((fe)=>{
+    console.log(fe);
+});
+
+//callback function
+
+function add(a, b) {
+    return a + b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+function cb(a, b, operation) {
+    return operation(a, b);
+}
+
+console.log(cb(2, 2, add));
+console.log(cb(2, 2, div));
+
+
+//callback example 2
+
+function greet(name, callback) {
+    console.log("Hello, " + name + "!");
+    callback(); // Call the callback function
+}
+
+function sayGoodbye() {
+    console.log("Goodbye!");
+}
+
+// Call the greet function with a callback function
+greet("John", sayGoodbye);
+
+
+
+function callback(name, callb){
+  console.log("hello" + " " + name);
+  callb();
+}
+
+function callb(){
+ console.log("how are you?")
+}
+
+callback("ram", callb);
+
+// ---------------------------------------------------------------//
+function callbac(name, gret){
+    console.log("hello" + " " + name);
+    gret();
+  }
+
+function gret(){
+    console.log("how are you?")
+   }
+
+callbac('vimal', gret);
+
+let spread = [1,2,3,4,5]
+
+let spread2 = [6,7,8,9,10];
+
+let combine = [...spread, ...spread2, 11,12]
+
+console.log(combine);
+
+function add(...rest){
+   return rest.reduce((accum,curr)=> accum + curr, 0)
+}
+console.log(add(6,8,7,7));
+
+console.log(`${spread} "hello" ${spread2}`)
+
+
+//reduce
+
+var numbers = [1, 2, 3, 4, 5];
+
+var sum = numbers.reduce(function(accumulator, currentValue) {
+    return accumulator + currentValue;
+}, 0);
+
+console.log(sum); // Output: 15 (1 + 2 + 3 + 4 + 5 = 15)
+
+
+var people = [
+    { name: "John", age: 30 },
+    { name: "Jane", age: 25 },
+    { name: "Doe", age: 35 }
+];
+
+people.sort(function(a, b) {
+    return a.age - b.age; // Sort by age in ascending order
+});
+
+console.log(people);
