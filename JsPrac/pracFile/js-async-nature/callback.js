a79d28bd5c3a6f45aec7e5bf7ca0ecb9be30f756
vimal336 <asvimal336@gmail.com>
2024-05-10 16:15:53 +0530

    js practice

--- JsPrac/pracFile/js-async-nature/callback.js
+++ JsPrac/pracFile/js-async-nature/callback.js
@@ -1,6 +1,7 @@
 function one(to){
   console.log("call one");
-  to()
+  to();
+ 
 }
 
 one(two);
@@ -8,6 +9,14 @@ one(two);
 function two(){
     console.log("call two");
 }
-  
 
 
+
+function execute(a,b,cb){
+    var add = a+b;
+    cb(add);
+}
+
+execute(10,20,function(add){
+   console.log(add/2);
+})
