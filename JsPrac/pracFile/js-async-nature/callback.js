8383a42ab374e671d42ed96c46aeb830f417e11f
vimal336 <asvimal336@gmail.com>
2024-05-13 10:58:59 +0530

    js practice

--- JsPrac/pracFile/js-async-nature/callback.js
+++ JsPrac/pracFile/js-async-nature/callback.js
@@ -22,11 +22,23 @@ execute(10,20,function(add){
 })
 
 
-setTimeout(function callback(){
-  console.log("execute");
-},3000)
 
-// setTimeout(callback, 3000);
+//callback hell
 
+setTimeout(()=>{
+  console.log("1");
+  setTimeout(()=>{
+    console.log("2");
+    setTimeout(()=>{
+      console.log("3");
+      setTimeout(()=>{
+        console.log("4");
+        setTimeout(()=>{
+          console.log("5");
+        },5000);
+      },4000);
+    },3000);
+  },2000);
+},1000);
 
 
