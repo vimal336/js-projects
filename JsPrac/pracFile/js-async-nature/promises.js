644fe5adfda1255d085638b7d4fd4fa2322920b5
vimal336 <asvimal336@gmail.com>
2024-05-13 12:40:56 +0530

    js practice

--- JsPrac/pracFile/js-async-nature/promises.js
+++ JsPrac/pracFile/js-async-nature/promises.js
@@ -1,17 +1,12 @@
-// const mypromise = new Promise((resolve, reject)=>{
-//   let randomnum = Math.floor(Math.random()*2);
-//   console.log(randomnum);
-//   if(randomnum == 0){
-//     resolve();
-//   } else{
-//     reject();
-//   }
-// });
-
-// mypromise.then( ()=>console.log("Success")).catch(()=> console.log("failure") )
-
-
-
-const jspromise =  new Promise(()=>{
-  
+const jspromise =  new Promise((resolve, reject)=>{
+  const rand = Math.floor(Math.random()*2);
+  console.log(rand);
+  if(rand == 0){
+    resolve();
+  } else{
+    reject();
+  }
 });
+
+jspromise.then(()=>console.log("Success")).catch(()=>console.log("failure"));
+
