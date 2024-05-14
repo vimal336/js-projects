4787d1b48764f6f235d05c44bcf0bfd8d1ccb567
vimal336 <asvimal336@gmail.com>
2024-05-14 12:32:03 +0530

    js practice

--- JsPrac/pracFile/JsConcepts/Dom/images/dom.js
+++ JsPrac/pracFile/JsConcepts/Dom/images/dom.js
@@ -3,8 +3,14 @@
 
 // We can also add events to these elements to make our page more dynamic.
 
+// browser gives DOM API functions like getElemenyById(), createElement() etc..
+
+// and this functions are not from javascript these all are provided by DOM API
+
 // The DOM views an HTML document as a tree of nodes. A node represents an HTML element.
 
 //Dom is not part of the js language but is instead a web api used to build websites.
 
 // dom was designed to be independent of any particular prog lang.
+
+
