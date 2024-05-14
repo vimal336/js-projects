ce05d0b06dba6c87cf6e77869d6bc7dbb135d85e
vimal336 <asvimal336@gmail.com>
2024-05-14 15:07:07 +0530

    js practice

--- JsPrac/pracFile/JsConcepts/default-parameters/defaultpara.js
+++ JsPrac/pracFile/JsConcepts/default-parameters/defaultpara.js
@@ -14,21 +14,3 @@ function multiply(a, b = 3) {
   // Expected output: 15
 
 
-  num1 = 10;
-  num2 = 20;
-
-  console.log(num2);
-
-  num2 = num1
-
-  console.log(num2);
-
-  console.log(num1);
-  console.log(num2);
-  
-
-  num1 = 30;
-
-  console.log(num1);
-
-  console.log(num2);
