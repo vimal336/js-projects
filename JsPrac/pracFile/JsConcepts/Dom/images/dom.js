378b2f5b5aa921c00a48c9897a11472a5169f5d1
vimal336 <asvimal336@gmail.com>
2024-05-14 11:46:31 +0530

    js practice

--- JsPrac/pracFile/JsConcepts/Dom/images/dom.js
+++ JsPrac/pracFile/JsConcepts/Dom/images/dom.js
@@ -1,1 +1,7 @@
+// DOM stands for Document Object Model. It is a programming interface 
+// that allows us to create, change, or remove elements from the document. 
+// We can also add events to these elements to make our page more dynamic.
+
+// The DOM views an HTML document as a tree of nodes. A node represents an HTML element.
+
 //Dom is not part of the js language but is instead a web api used to build websites.
