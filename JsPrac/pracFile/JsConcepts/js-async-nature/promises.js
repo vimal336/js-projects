dabd43f7afe97e39c213117619808812a1e3c20b
vimal336 <asvimal336@gmail.com>
2024-05-18 18:55:23 +0530

    JS DOM practice

--- JsPrac/pracFile/JsConcepts/js-async-nature/promises.js
+++ JsPrac/pracFile/JsConcepts/js-async-nature/promises.js
@@ -1,49 +1,55 @@
-//This imports the Node.js built-in fs module, which provides file system-related functionality.
-const fs = require('fs');
+// //This imports the Node.js built-in fs module, which provides file system-related functionality.
+// const fs = require('fs');
 
-// imports axios
-const axios = require('axios');
-
-
-// file read using promise method
-fs.promises.readFile('./text.txt',{encoding: 'utf-8'}).then( (data)=> console.log(data) ).catch( (error)=>  console.log(error));
+// // imports axios
+// const axios = require('axios');
 
 
-// file read using callback method
-fs.readFile('./text.txt', 'utf8', (error, data) => {
-  if (error) {
-    console.error('Error reading file:', error);
-    return;
+// // file read using promise method
+// fs.promises.readFile('./text.txt',{encoding: 'utf-8'}).then( (data)=> console.log(data) ).catch( (error)=>  console.log(error));
+
+
+// // file read using callback method
+// fs.readFile('./text.txt', 'utf8', (error, data) => {
+//   if (error) {
+//     console.error('Error reading file:', error);
+//     return;
+//   }
+//   console.log('File content:', data);
+// });
+
+// //promise method example 1 :
+// const jspromise =  new Promise((resolve, reject)=>{
+//   const rand = Math.floor(Math.random()*2);
+//   console.log(rand);
+//   if(rand == 0){
+//     resolve();
+//   } else{
+//     reject();
+//   }
+// });
+
+// jspromise.then(()=>console.log("Success")).catch(()=>console.log("failure"));
+
+
+
+// //Axios Promise
+
+// const url = 'https://jsonplaceholder.typicode.com/users/'
+
+// fs.promises.readFile('./text.txt', {encoding:'utf-8'}).then((data) => console.log(data)).catch((error) => console.log(error));
+
+// axios.get(url+1).then( (data)=>console.log(data.data.name) )
+
+
+
+const prom = new Promise((resolve,reject)=>{
+  if(true){
+    resolve()
+  }else{
+    reject()
   }
-  console.log('File content:', data);
-});
-
-//promise method example 1 :
-const jspromise =  new Promise((resolve, reject)=>{
-  const rand = Math.floor(Math.random()*2);
-  console.log(rand);
-  if(rand == 0){
-    resolve();
-  } else{
-    reject();
-  }
-});
-
-jspromise.then(()=>console.log("Success")).catch(()=>console.log("failure"));
-
-
-
-//Axios Promise
-
-const url = 'https://jsonplaceholder.typicode.com/users/'
-
-fs.promises.readFile('./text.txt', {encoding:'utf-8'}).then((data) => console.log(data)).catch((error) => console.log(error));
-
-axios.get(url+1).then( (data)=>console.log(data.data.name) )
-
-
-
-const prom = new Promise(()=>{
-  
 })
 
+prom.then("resolve");
+prom.catch("rejected");
