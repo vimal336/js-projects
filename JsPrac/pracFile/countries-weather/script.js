263ad3a5a251aa3aec239a9b51c247c9a7aa92cd
vimal336 <asvimal336@gmail.com>
2024-05-20 15:42:14 +0530

    JS DOM practice

--- JsPrac/pracFile/countries-weather/script.js
+++ JsPrac/pracFile/countries-weather/script.js
@@ -4,13 +4,19 @@ function fetchData() {
   let data1 =""
   countries.map((values)=>{
     data1+=`<div class="weather-box">
-    <div class="weather-title">
+  <div class="weather-head">
+       <div class="weather-title">
+       <h3>${values.name.common}</h3>
+     </div>
         <div class="weather-img">
             <img src=${values.flags.png}>
         </div>
-        <h3>${values.name.common}</h3>
     </div>
-    <P>region: ${values.region}</P>
+    <P><b>region:</b> ${values.region}</P>
+    <P><b>capital:</b> ${values.capital}</P>
+    <P><b>country code:</b> ${values.altSpellings[0]} </P>
+    <P><b>Latlng</b>: ${values.latlng[0]},${values.latlng[1]}</P>
+    <button class="weather-btn">Click for weather</button>
 </div>`
   })
   document.querySelector(".weather-container").innerHTML=data1;
@@ -25,7 +31,8 @@ async function fetchata() {
       const response = await fetch('https://restcountries.com/v3.1/all')
       const data = await response.json();
       console.log(data);
-  } catch (error) {
+  } catch (
+    error) {
       console.error('Error fetching data:', error);
   }
 }
