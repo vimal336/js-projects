95f6920b9986ccb40187e8899a0da88043b0b8fe
vimal336 <asvimal336@gmail.com>
2024-05-20 13:03:43 +0530

    JS DOM practice

--- JsPrac/pracFile/countries-weather/script.js
+++ JsPrac/pracFile/countries-weather/script.js
@@ -1,37 +1,41 @@
 async function fetchData() {
-  try {
-      const response = await fetch('https://restcountries.com/v3.1/all')
-      const data = await response.json();
-      return data;
-  } catch (error) {
-      console.error('Error fetching data:', error);
-  }
+ fetch("https://restcountries.com/v3.1/all").then
+ (res => res.json()).then((countries)=>{
+  let data1 =""
+  countries.map(()=>{
+    data1=``
+  })
+ })
 }
 
+fetchData();
 
-async function renderData() {
-  const container = document.querySelector('.container');
-  const data = await fetchData();
 
-  if (!data) {
-      return;
-  }
 
-  data.forEach(item => {
-      const card = document.createElement('div');
-      card.classList.add('card');
 
-      const title = document.createElement('h2');
-      title.textContent = item.title;
+// async function renderData() {
+//   const container = document.querySelector('.container');
+//   const data = await fetchData();
 
-      const body = document.createElement('p');
-      body.textContent = item.body;
+//   if (!data) {
+//       return;
+//   }
 
-      card.appendChild(title);
-      card.appendChild(body);
-      container.appendChild(card);
-  });
-}
+//   data.forEach(item => {
+//       const card = document.createElement('div');
+//       card.classList.add('card');
+
+//       const title = document.createElement('h2');
+//       title.textContent = item.title;
+
+//       const body = document.createElement('p');
+//       body.textContent = item.body;
+
+//       card.appendChild(title);
+//       card.appendChild(body);
+//       container.appendChild(card);
+//   });
+// }
 
 // Call the renderData function to display data
-renderData();
+// renderData();
