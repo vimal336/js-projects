8e6cd453b0434e25ef76e3f65c8b5acce5151347
vimal336 <asvimal336@gmail.com>
2024-05-29 12:56:35 +0530

    javascript oops

--- JsPrac/pracFile/JsConcepts/js-oops/objects/object.js
+++ JsPrac/pracFile/JsConcepts/js-oops/objects/object.js
@@ -10,8 +10,30 @@ class car{
         this.price = price;
         this.color = color;
     }
+
+    getname = function(){
+        console.log(this.model);
+    }
 }
 
 let car1 = new car("bmw","2c","white");
 
 console.log(car1.price);
+car1.getname();
+
+
+//super keyword
+
+class schoolstudents{
+    constructor(student,age,std){
+        this.student = student;
+        this.age = age;
+        this.std = std;
+    }
+}
+
+class school extends schoolstudents(){
+    
+}
+
+
