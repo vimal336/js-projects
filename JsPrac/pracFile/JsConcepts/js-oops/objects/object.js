cb962890f344ef02ad99c806789b1b93db962ac1
vimal336 <asvimal336@gmail.com>
2024-05-29 10:39:11 +0530

    javascript oops

--- JsPrac/pracFile/JsConcepts/js-oops/objects/object.js
+++ JsPrac/pracFile/JsConcepts/js-oops/objects/object.js
@@ -1,1 +1,13 @@
 //Object
+
+class car{
+    constructor(model,price,color){
+        this.model = model;
+        this.price = price;
+        this.color = color;
+    }
+}
+
+let car1 = new car("bmw","2c","white");
+
+console.log(car1);
