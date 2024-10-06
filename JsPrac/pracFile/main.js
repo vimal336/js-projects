85645c9bdb52e52ba376e29c29a6a2a4a9afc983
vimal336 <asvimal336@gmail.com>
2024-10-06 21:19:06 +0530

    js

--- JsPrac/pracFile/main.js
+++ JsPrac/pracFile/main.js
@@ -311,4 +311,4 @@ if ("") console.log("Falsy");
 // Truthy
 if (true) console.log("Truthy");
 if (1) console.log("Truthy");
-if ("hello") console.log("Truthy");
+if ("helloworld") console.log("Truthy");
