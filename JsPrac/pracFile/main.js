362610db62df7d6aca70160b77da97c27623c786
vimal336 <asvimal336@gmail.com>
2024-10-11 17:26:47 +0530

    js

--- JsPrac/pracFile/main.js
+++ JsPrac/pracFile/main.js
@@ -335,3 +335,7 @@ promise
   const numbers = [1, 2, 3, 4];
 
   numbers.forEach(num => console.log(num * 2)); // Logs: 2, 4, 6, 8
+
+  const numbers = [1, 2, 3, 4];
+const doubled = numbers.map(num => num * 2);
+console.log(doubled); // Output: [2, 4, 6, 8]
