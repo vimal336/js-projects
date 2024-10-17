3a58b9f3becc0463e0f35a14b727a7f644bb2dd0
vimal336 <asvimal336@gmail.com>
2024-10-17 10:51:21 +0530

    js

--- JsPrac/pracFile/main.js
+++ JsPrac/pracFile/main.js
@@ -341,7 +341,7 @@ const doubled = numbers.map(num => num * 2);
 console.log(doubled); // Output: [2, 4, 6, 8]
 
 
-const array = ['apple', 'banana', 'cherry'];
+const array = ['apple', 'guava' 'banana', 'cherry'];
 
 for (const element of iterable) {
   
