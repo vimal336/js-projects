4c19cb4d1d8a59540944eb69433695259236706a
vimal336 <asvimal336@gmail.com>
2024-10-30 22:46:10 +0530

    js

--- JsPrac/pracFile/main.js
+++ JsPrac/pracFile/main.js
@@ -377,7 +377,7 @@ for (let key in person) {
 
 }
 
-const person = { name: "Alice", age: 25 };
+const person = { name: "vimal", age: 25 };
 for (const key in person) {
    console.log(key, person[key]); // Outputs "name Alice" and "age 25"
 }
