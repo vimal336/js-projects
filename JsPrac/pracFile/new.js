0329c5676992c5862a02bd75b5599af001072c1a
vimal336 <asvimal336@gmail.com>
2024-12-31 17:56:41 +0530

    js

--- JsPrac/pracFile/new.js
+++ JsPrac/pracFile/new.js
@@ -10,3 +10,17 @@ checkEven
     .catch((error) => console.error(error)); // On failure
 
     .then((message) => console.log(message)) 
+
+
+    let checkEven = new Promise((resolve, reject) => {
+        let number = 4;
+    
+        if (number % 2 === 0) resolve("The number is even!");
+        else reject("The number is odd!");
+    });
+    
+    checkEven
+        .then((message) => console.log(message)) // On success
+        .catch((error) => console.error(error)); // On failure
+    
+        .then((message) => console.log(message)) 
