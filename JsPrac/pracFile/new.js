9ea02d1afffd3044e68f8b2c072af6a551ac6833
vimal336 <asvimal336@gmail.com>
2025-01-09 16:25:47 +0530

    js

--- JsPrac/pracFile/new.js
+++ JsPrac/pracFile/new.js
@@ -21,4 +21,7 @@ checkEven
         .then((msg) => console.log(msg)) // On success
         .catch((error) => console.error(error)); // On failure
 
+        checkEven
+        .then((msg) => console.log(msg)) // On success
+        .catch((error) => console.error(error)); // On failure
   
