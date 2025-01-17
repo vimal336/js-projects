0af9aab20a25dc48ac2f489cf05037f8d227be1e
vimal336 <asvimal336@gmail.com>
2025-01-17 12:30:39 +0530

    js

--- JsPrac/pracFile/new.js
+++ JsPrac/pracFile/new.js
@@ -43,5 +43,5 @@ function isBigEnough(value) {
   return value >= 10;
 }
 
-const filtered = [12, 5, 8, 130, 44].filter(isBigEnough);
+const filtered = [12, 45, 8, 130, 44].filter(isBigEnough);
 // filtered is [12, 130, 44]
