96f3c1c5f5fe6184f29a5d336bcb39826fa7cc02
vimal336 <asvimal336@gmail.com>
2025-01-23 17:27:26 +0530

    js

--- JsPrac/pracFile/new.js
+++ JsPrac/pracFile/new.js
@@ -50,3 +50,14 @@ function isSmall(value) {
     return value >= 6;
   }
 
+  function isPalindrome(str) {
+    let rev = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        rev += str[i];
+    }
+    if (rev == str) {
+        return true
+    } else {
+        return false;
+    }
+}
