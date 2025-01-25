d638a39ec406ffb54c0081f8658cd806a1c3d9fe
vimal336 <asvimal336@gmail.com>
2025-01-25 16:57:11 +0530

    js

--- JsPrac/pracFile/new.js
+++ JsPrac/pracFile/new.js
@@ -82,3 +82,8 @@ fetch(apiUrl)
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
