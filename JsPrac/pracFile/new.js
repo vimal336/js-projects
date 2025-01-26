11672c77e2bdd6f3c52e472d1965fd6673d7da0c
vimal336 <asvimal336@gmail.com>
2025-01-26 22:53:39 +0530

    js

--- JsPrac/pracFile/new.js
+++ JsPrac/pracFile/new.js
@@ -74,7 +74,7 @@ console.log(isPalindrome(str4));
 
 
 // Define the API URL
-const apiUrl = '';
+const apiUrl = "https://jsonplaceholder.typicode.com/posts";
 
 // Make a GET request
 fetch(apiUrl)
