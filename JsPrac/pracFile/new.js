c148849fb91f27447cee8b0e034859872a63400f
vimal336 <asvimal336@gmail.com>
2025-01-27 18:06:08 +0530

    js

--- JsPrac/pracFile/new.js
+++ JsPrac/pracFile/new.js
@@ -90,3 +90,21 @@ fetch(apiUrl)
   .catch(error => {
     console.error('Error:', error);
   });
+
+
+  const apiUrl = "https://jsonplaceholder.typicode.com/posts";
+
+// Make a GET request
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
