c4925957db0445ba0485a8a19cccd6196a243c3e
vimal336 <asvimal336@gmail.com>
2025-01-30 19:09:47 +0530

    js

--- JsPrac/pracFile/new.js
+++ JsPrac/pracFile/new.js
@@ -92,7 +92,7 @@ fetch(apiUrl)
   });
 
 
-  const apiUrl2 = "https://jsonplaceholder.typicode.com";
+  const apiUrl2 = "https://jsonplaceholder.typicode.com/";
 
 // Make a GET request
 fetch(apiUrl2)
